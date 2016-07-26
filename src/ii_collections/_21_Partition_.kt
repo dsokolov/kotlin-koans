de76c6c892d29fc8bb5d25fb0387ebbdd0ef1a40
dmitry.i.sokolov <DmitryIlichSokolov@gmail.com>
2016-07-26 17:13:40 +0400

    part ii

--- src/ii_collections/_21_Partition_.kt
+++ src/ii_collections/_21_Partition_.kt
@@ -10,7 +10,5 @@ fun example8() {
     negative == listOf(-4, -11)
 }
 
-fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> {
-    // Return customers who have more undelivered orders than delivered
-    todoCollectionTask()
-}
+// Return customers who have more undelivered orders than delivered
+fun Shop.getCustomersWithMoreUndeliveredOrdersThanDelivered(): Set<Customer> = this.customers.partition { it.orders.count { it.isDelivered } >= it.orders.count { !it.isDelivered }  }.second.toSet()
