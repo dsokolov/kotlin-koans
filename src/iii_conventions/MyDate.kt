2c021838190ceb15f8d2e91ba128d950847c6fe9
dmitry.i.sokolov <DmitryIlichSokolov@gmail.com>
2017-05-05 10:40:53 +0400

    sadfsdgfdf

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,45 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Iterator<MyDate> {
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    override fun hasNext(): Boolean = true
+
+    override fun next(): MyDate = this.nextDate()
+
+}
+
+operator fun MyDate.plus(ti: TimeInterval): MyDate = when (ti) {
+    TimeInterval.DAY -> MyDate(this.year, this.month, this.dayOfMonth + 1)
+    TimeInterval.WEEK -> MyDate(this.year, this.month, this.dayOfMonth + 7)
+    TimeInterval.YEAR -> MyDate(this.year + 1, this.month, this.dayOfMonth)
+    else -> MyDate(this.year, this.month, this.dayOfMonth)
+}
+
+fun MyDate.nextDate() = MyDate(this.year, this.month, this.dayOfMonth + 1)
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.compareTo(other: MyDate): Int {
+    if (this.year > other.year) {
+        return 1
+    } else if (this.year < other.year) {
+        return -1
+    } else {
+        if (this.month > other.month) {
+            return 1
+        } else if (this.month < other.month) {
+            return -1
+        } else {
+            if (this.dayOfMonth > other.dayOfMonth) {
+                return 1
+            } else if (this.dayOfMonth < other.dayOfMonth) {
+                return -1
+            } else {
+                return 0
+            }
+        }
+    }
+}
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +47,10 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) {
+
+}
+
+operator fun DateRange.contains(date: MyDate): Boolean = date > this.start && date <= this.endInclusive
+
+operator fun DateRange.iterator(): Iterator<MyDate> = MyDate(this.start.year, this.start.month, this.start.dayOfMonth)
